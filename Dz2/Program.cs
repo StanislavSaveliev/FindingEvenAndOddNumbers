9a3dbcad19997b461ce54b3c35aaf6140cb5065a
Stanislav Saveliev <texnikum2288@gmail.com>
2024-12-12 22:29:12 +0300

    Добавьте файлы проекта.

---
+++ Dz2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;            
+            a = int.Parse(Console.ReadLine());
+            int b = a % 2;
+            bool resulr = b==0 ;
+
+            if (resulr)
+            {
+                Console.WriteLine("Ваше число является четным");
+            }
+
+            else
+            {
+                Console.WriteLine("Ваше число не является четным");
+            }
+              
+        }
+    }
+}
